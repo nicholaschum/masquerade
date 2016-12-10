a089704b4685057070e1ec96de849b7295dd7578
Surge1223 <surge1223@gmail.com>
2016-12-10 15:10:16 -0600

    Add our own permission.xml file
    will be used in the future for feature separation
    gets added inside /system/etc/permissions

--- Android.mk
+++ Android.mk
@@ -13,11 +13,21 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+LOCAL_REQUIRED_MODULES := projekt.substratum.theme.xml
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 
+LOCAL_SDK_VERSION := current
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := projekt.substratum.theme.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
